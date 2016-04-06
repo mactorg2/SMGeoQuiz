b5e61464ca909d39dc3db533ae2782859a4eec35
Mactorg2 <seanatsushimonahan@gmail.com>
2016-04-05 19:23:57 -0700

    Added in additional code to make Previous Button work, with lots of help from Micah.

--- app/src/main/java/com/example/sean/smgeoquiz/QuizActivity.java
+++ app/src/main/java/com/example/sean/smgeoquiz/QuizActivity.java
@@ -77,7 +77,9 @@ public class QuizActivity extends AppCompatActivity {
         mPrevButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                mCurrentIndex = (mCurrentIndex - 1) % mQuestionBank.length;
+                mCurrentIndex = (mCurrentIndex == 0) ? mQuestionBank.length - 1 : (mCurrentIndex - 1);
+
+
                 updateQuestion();
             }
         });
